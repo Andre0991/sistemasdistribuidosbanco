892e5072cffd9a1b93d3e72bc3c22fed99589f49
Andre0991 <andre.peric.tavares@gmail.com>
2017-07-23 19:16:21 -0300

    agora vai

--- src/main/java/com/mkyong/rest/HelloWorldService.java
+++ src/main/java/com/mkyong/rest/HelloWorldService.java
@@ -12,7 +12,7 @@ public class HelloWorldService {
 	@Path("/{param}")
 	public Response getMsg(@PathParam("param") String msg) {
  
-		String output = "Teste: modificado - mensgem: " + msg;
+		String output = "novo: " + msg;
  
 		return Response.status(200).entity(output).build();
  
