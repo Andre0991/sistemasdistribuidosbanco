9dfc137da2b9512e26ca176ede697b7659506293
pracchias <iasmin.haro@gmail.com>
2017-08-08 01:06:28 -0300

    possibilidade de filtrar TED/Saque/Transferencia por Cliente

--- src/main/java/dao/LogTransferenciaDAO.java
+++ src/main/java/dao/LogTransferenciaDAO.java
@@ -3,7 +3,11 @@ package dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.mkyong.entidades.Cliente;
+import com.mkyong.entidades.LogSaque;
 import com.mkyong.entidades.LogTransferencia;
+
+import factory.Singleton;
 
 
 public class LogTransferenciaDAO {
@@ -13,6 +17,30 @@ public class LogTransferenciaDAO {
 		public List<LogTransferencia> findAll() {
 			return historicoTransferencia;
 		}
+		
+		
+		//traz as transferencias que envolvem o id, seja ele sender ou receiver
+		public List<LogTransferencia> findByCliente(final long id) {
+			ClienteDAO clienteDAO = Singleton.INSTANCE.getClienteDAO();
+			Cliente cliente = clienteDAO.findById(id);
+			if (cliente == null) {
+				return null;
+			}
+			String numeracaoContaCorrente = cliente.getContaCorrente().getNumeracao();
+			List<LogTransferencia> transferenciasContaCorrentesFromCliente = new ArrayList<LogTransferencia>();
+			for (LogTransferencia logTransferencias : historicoTransferencia) {
+				//se for o receiver 
+				if (logTransferencias.getContaDestino().equals(numeracaoContaCorrente)) {
+					transferenciasContaCorrentesFromCliente.add(logTransferencias);
+				}
+				//se for o sender
+				else if(logTransferencias.getContaOrigem().equals(numeracaoContaCorrente)) {
+					transferenciasContaCorrentesFromCliente.add(logTransferencias);
+				}
+			}
+			return transferenciasContaCorrentesFromCliente;
+		}
+		
 		
 		public void add(final LogTransferencia logTransferencia) {
 			this.historicoTransferencia.add(logTransferencia);
